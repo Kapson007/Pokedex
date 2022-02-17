a30eaf157c9a21e5143a828e3e32b7453866e9df
Kacper Nadstoga <kapix007@gmail.com>
2022-02-17 19:07:23 +0100

    add ts-loader

--- Pokedex/webpack.config.js
+++ Pokedex/webpack.config.js
@@ -1,12 +1,28 @@
 const path = require("path");
 
 module.exports = {
+    context: __dirname,
     mode: "development",
     entry:{
-        main: "./src/main.js",
+        main: "./src/main.ts",
     },
     output: {
         filename: "js/app.js",
-        path: path.resolve(__dirname, "./", "dist"),
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist/"
+    },
+    module: {
+        rules:[
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: "ts-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts"]
     }
 }
