e538c084b0d4327b1c52022e13074e7694f72f6b
Kacper Nadstoga <kapix007@gmail.com>
2022-02-17 18:18:33 +0100

    create webpack.config.js set basic conf, create dist folder and index.html

---
+++ Pokedex/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry:{
+        main: "./src/main.js",
+    }
+    output: {
+        filename: "js/app.js",
+        path: path.resolve(__dirname, "../", "dist"),
+    }
+}
