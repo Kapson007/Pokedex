f35748b4ab453b983d929784780fe60601f75166
Kacper Nadstoga <kapix007@gmail.com>
2022-03-01 22:35:29 +0100

    add last improvements

--- Pokedex/src/ts/closeLoader.ts
+++ Pokedex/src/ts/closeLoader.ts
@@ -1,5 +1,5 @@
 export const closeLoader = (): void => {
     const loader =  document.querySelector('.app__loader') as HTMLDivElement;
     loader?.parentElement?.removeChild(loader);
-    console.log('deleted');
+    // console.log('deleted');
 }
